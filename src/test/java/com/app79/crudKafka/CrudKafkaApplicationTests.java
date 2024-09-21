59da8c58ca5d081e46c89df4942000cd0f492d5d
Sahrul1610 <sahrulfyou@gmail.com>
2024-09-21 20:52:33 +0700

    first

---
+++ src/test/java/com/app79/crudKafka/CrudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app79.crudKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
